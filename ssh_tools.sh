f5357131563cfec2fed460bd207f03e72e68d123
sohumgautam17 <69330896+sohumgautam17@users.noreply.github.com>
2024-08-21 15:23:33 -0400

    Update ssh_tools.sh

--- ssh_tools.sh
+++ ssh_tools.sh
@@ -9,3 +9,7 @@ cat ~/.ssh/id_ed25519.pub
 
 # Go to github and add ssh key 
 ssh -T git@github.com
+
+# Update the Remote URL to Use SSH:
+git remote set-url origin git@github.com:username/repository.git
+
