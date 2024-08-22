41bb293d2b1e89a5073a74d16eb36f0d4dc0d207
sohumgautam17 <69330896+sohumgautam17@users.noreply.github.com>
2024-08-22 15:03:53 -0400

    Update ssh_tools.sh

--- ssh_tools.sh
+++ ssh_tools.sh
@@ -7,6 +7,13 @@ ssh-add ~/.ssh/id_ed25519
 
 cat ~/.ssh/id_ed25519.pub
 
+#list keys added to agent
+ssh-add -l
+
+# verify permissions
+chmod 600 ~/.ssh/id_rsa
+chmod 644 ~/.ssh/id_rsa.pub
+
 # Go to github and add ssh key 
 ssh -T git@github.com
 
