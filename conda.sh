118811906ff101c61767bd3a07daeb9998b79a02
sohumgautam17 <69330896+sohumgautam17@users.noreply.github.com>
2025-02-05 16:49:29 -0500

    Update conda.sh

--- conda.sh
+++ conda.sh
@@ -1,3 +1,8 @@
+# Install Conda for Linux 
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+chmod +x Miniconda3-latest-Linux-x86_64.sh
+./Miniconda3-latest-Linux-x86_64.sh
+
 conda create --name <enter env name> python=3.9
 
 conda activate <env_name>
